8a331332fbcfec7ad2d2cf2c2a98da20857a1e3e
Kate Kumpanska <kate.kumpansky@gmail.com>
2025-12-13 16:39:20 +0200

    test: add unit tests for CalculateTotalPrice, AddItem, RemoveItem

--- UnitTesting/OrderTests.cs
+++ UnitTesting/OrderTests.cs
@@ -126,4 +126,78 @@ public class Tests
                                         .Build();
          Assert.That(() => order.ValidateOrder(), Throws.Exception.TypeOf<ArgumentException>());
     }
+    [Test]
+    public void CalculateTotalPrice_ItemsPriceAbove500_DeliveryPriceIsZero()
+    {
+        List<IFood> items = new() { new Carbonara(), new PhiladelphiaRoll(), new TomYumSoup(), new PizzaDiablo() };
+        Order order = new OrderBuilder().WithId(1)
+                                        .WithRestaurant(1)
+                                        .WithItems(items)
+                                        .WithDeliveryAdress("Test Address")
+                                        .WithCustomerId(1)
+                                        .WithCustomerNumber("+380978312233")
+                                        .Build();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(order.DeliveryPrice, Is.EqualTo(0));
+            Assert.That(order.TotalPrice, Is.EqualTo(119.99 + 129.99 + 149.99 + 149.99 + order.DeliveryPrice));
+        });
+    }
+
+    [Test]
+    public void CalculateTotalPrice_ItemsPriceBetween350And500_DeliveryPriceIs50ButTotalDoesntEqual()
+    {
+        List<IFood> items = new() { new Carbonara(), new TomYumSoup(), new PizzaDiablo() };
+        Order order = new OrderBuilder().WithId(1)
+                                        .WithRestaurant(1)
+                                        .WithItems(items)
+                                        .WithDeliveryAdress("Test Address")
+                                        .WithCustomerId(1)
+                                        .WithCustomerNumber("+380978312233")
+                                        .Build();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(order.DeliveryPrice, Is.EqualTo(50));
+            Assert.That(order.TotalPrice, Is.Not.EqualTo(119.99 + 149.99 + 149.99 + 70));
+        });
+    }
+
+    [Test]
+    public void AddItem_PriceChanged_TotalEquals()
+    {
+        List<IFood> items = new() { new OnionSoup() };
+        Order order = new OrderBuilder().WithId(1)
+                                        .WithRestaurant(1)
+                                        .WithItems(items)
+                                        .WithDeliveryAdress("Test Address")
+                                        .WithCustomerId(1)
+                                        .WithCustomerNumber("+380978312233")
+                                        .Build();
+        PizzaMeatLovers pizza = new PizzaMeatLovers();
+        order.AddItem(pizza);
+
+        Assert.That(order.TotalPrice, Is.EqualTo(154.99 + 99.99 + order.DeliveryPrice));
+    }
+
+    [Test]
+    public void RemoveItem_TotalBelow200_DeliveryPriceEquals100()
+    {
+        PizzaDiablo item1 = new();
+        PhiladelphiaRoll item2 = new();
+
+        List<IFood> items = new() { item1, item2 };
+        Order order = new OrderBuilder().WithId(1)
+                                        .WithRestaurant(1)
+                                        .WithItems(items)
+                                        .WithDeliveryAdress("Test Address")
+                                        .WithCustomerId(1)
+                                        .WithCustomerNumber("+380978312233")
+                                        .Build();
+
+        Assert.That(order.DeliveryPrice, Is.EqualTo(75));
+        order.RemoveItem(item2);
+        Assert.That(order.DeliveryPrice, Is.EqualTo(100));
+    }
 }
