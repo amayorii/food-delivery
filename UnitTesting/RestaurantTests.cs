86fb9a46a150e139fb76d8caaae75b6ef3ef188a
Nazariy <malashenko.naz@gmail.com>
2025-12-14 23:13:33 +0200

    test: add RestaurantTests file & some mock tests for ServeOrder method

---
+++ UnitTesting/RestaurantTests.cs
@@ -0,0 +1,76 @@
+using food_delivery;
+using food_delivery.Food;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTesting;
+
+public class RestaurantTests
+{
+    private Mock<IKitchen> kitchenMock;
+    private Mock<ICourier> courierMock;
+    private Restaurant restaurant;
+
+    [SetUp]
+    public void Setup()
+    {
+        kitchenMock = new Mock<IKitchen>();
+        courierMock = new Mock<ICourier>();
+
+        courierMock.Setup(c => c.Bag).Returns(new List<IFood>());
+        courierMock.Setup(c => c.Status).Returns("Available");
+        courierMock.Setup(c => c.Id).Returns(50);
+
+        restaurant = new Restaurant(kitchenMock.Object);
+    }
+
+    [Test]
+    public void ServeOrder_KitchenHasIngredients_FoodInBag()
+    {
+        kitchenMock.Setup(k => k.HasIngredients(It.IsAny<string>())).Returns(true);
+
+        kitchenMock.Setup(k => k.HasFreeCooks()).Returns(true);
+
+        kitchenMock.Setup(k => k.CookDish("Carbonara")).Returns(new Carbonara());
+
+        Order order = new OrderBuilder().WithId(1)
+                                        .WithRestaurant(1)
+                                        .WithItems(new List<IFood> { new Carbonara() })
+                                        .WithDeliveryAdress("Test Address")
+                                        .WithCustomerId(1)
+                                        .WithCustomerNumber("+380978312233")
+                                        .Build();
+
+        restaurant.TakeOrder(order);
+        restaurant.AssignCourier(courierMock.Object);
+
+        restaurant.ServeOrder();
+
+        Assert.That(restaurant.Courier.Bag, Has.Some.InstanceOf<Carbonara>());
+    }
+
+    [Test]
+    public void ServeOrder_KitchenHasNoIngredients_ThrowsExc()
+    {
+        kitchenMock.Setup(k => k.HasIngredients(It.IsAny<string>())).Returns(true);
+
+        kitchenMock.Setup(k => k.HasFreeCooks()).Returns(true);
+
+        kitchenMock.Setup(k => k.HasIngredients("PizzaDiablo")).Returns(false);
+
+        Order order = new OrderBuilder().WithId(1)
+                                        .WithRestaurant(1)
+                                        .WithItems(new List<IFood> { new PizzaDiablo() })
+                                        .WithDeliveryAdress("Test Address")
+                                        .WithCustomerId(1)
+                                        .WithCustomerNumber("+380978312233")
+                                        .Build();
+
+        restaurant.TakeOrder(order);
+        restaurant.AssignCourier(courierMock.Object);
+
+        var ex = Assert.Throws<InvalidOperationException>(() => restaurant.ServeOrder());
+
+        Assert.That(ex.Message, Does.Contain("Missing ingredients for PizzaDiablo"));
+    }
+}
