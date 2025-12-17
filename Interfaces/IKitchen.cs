5239644e41c9b0005d458f89e71d1e7de886dc82
kumpanska <151912402+kumpanska@users.noreply.github.com>
2025-12-17 17:50:43 +0200

    Fd 0027 implement kitchen class (#77)
    * feat: add Restock, GetInventory methods and FreeCooks property to IKitchen
    
    * feat: implement Kitchen class

--- Interfaces/IKitchen.cs
+++ Interfaces/IKitchen.cs
@@ -7,4 +7,7 @@ public interface IKitchen
     public IFood CookDish(string name);
     public bool HasIngredients(string name);
     public bool HasFreeCooks();
+    public void Restock(string name, int quantity);
+    public Dictionary<string, int> GetInventory();
+    int FreeCooks { get; }
 }
